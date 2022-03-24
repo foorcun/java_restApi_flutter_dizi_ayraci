c2219b9b13517f5326281a54beb238f82faab577
foorcun <msnifoorcun@hotmail.com>
2022-03-25 00:01:27 +0300

    entities Dizi, dataya göre yaratıldı.

---
+++ src/main/java/com/troyatech/dizi_ayraci/entities/Dizi.java
@@ -0,0 +1,45 @@
+package com.troyatech.dizi_ayraci.entities;
+
+
+//{
+//    "id":1,
+//     "diziName":"Attack on Titan",
+//
+//        }
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="city")
+public class Dizi {
+    @Id
+    @Column(name="ID")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    @Column(name="diziName")
+    private String diziName;
+
+    public  Dizi(){}
+
+    public Dizi(int id, String diziName) {
+        this.id = id;
+        this.diziName = diziName;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDiziName() {
+        return diziName;
+    }
+
+    public void setDiziName(String diziName) {
+        this.diziName = diziName;
+    }
+}
